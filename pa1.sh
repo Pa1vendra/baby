cd60f71b8ca655d559fb32bafd08e4931d1b493a
root <root@ip-10-0-0-100.us-west-1.compute.internal>
2022-05-10 18:37:57 +0000

    commit by pavan

---
+++ pa1.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+echo "hii bujji..."
+sleep 2
+echo "nitho okati cheppali ra..."
+sleep 2
+I=0
+while [ $I -lt 10 ]
+do
+	echo "miss you my love..."
+	sleep 2
+	I=$(( $I + 1 ))
+done
+	sleep 2
+	echo "chudalani vundhi ra..."
+	sleep 2
+	echo "ravochu ga okasari na daagaraki..."
+	sleep 2
+        echo "wait chasthuna nikosam..."
+	sleep 2
+        echo "sarey na..."
+	sleep 2
+	echo "eka bajjo..."
+	sleep 2
+	echo "love you pilloda..."
+	sleep 2
+	echo "from your love..."
